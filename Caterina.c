ca4bdbc72733f4fdec975481ea4e3a0cfb501733
Jesse Vincent <jesse@keyboard.io>
2017-08-12 12:48:45 -0700

    Fix AVR910 typo to AVR109 per upstream fix:
    https://github.com/abcminiuser/lufa/commit/75419b4d97be051f0a1401e94f41121d24dbda44

--- Caterina.c
+++ Caterina.c
@@ -305,9 +305,9 @@ void EVENT_USB_Device_ControlRequest(void) {
 
 #if !defined(NO_BLOCK_SUPPORT)
 /** Reads or writes a block of EEPROM or FLASH memory to or from the appropriate CDC data endpoint, depending
- *  on the AVR910 protocol command issued.
+ *  on the AVR109 protocol command issued.
  *
- *  \param[in] Command  Single character AVR910 protocol command indicating what memory operation to perform
+ *  \param[in] Command  Single character AVR109 protocol command indicating what memory operation to perform
  */
 static void ReadWriteMemoryBlock(const uint8_t Command) {
     uint16_t BlockSize;
@@ -469,7 +469,7 @@ static void WriteNextResponseByte(const uint8_t Response) {
 #define STK_READ_PAGE       0x74  // 't'
 #define STK_READ_SIGN       0x75  // 'u'
 
-/** Task to read in AVR910 commands from the CDC data OUT endpoint, process them, perform the required actions
+/** Task to read in AVR109 commands from the CDC data OUT endpoint, process them, perform the required actions
  *  and send the appropriate response back to the host.
  */
 void CDC_Task(void) {
